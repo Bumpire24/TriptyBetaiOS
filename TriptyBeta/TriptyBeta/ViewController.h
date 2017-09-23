76c11981a2775bfc455c4bf29f8b49f20dd4624e
miguelfontamillas <miguelfontamillas@gmail.com>
2017-09-23 19:34:25 +0800

    Initial Commit

---
+++ TriptyBeta/TriptyBeta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TriptyBeta
+//
+//  Created by Miguel Fontamillas on 23/09/2017.
+//  Copyright © 2017 Miguel Fontamillas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TriptyBeta/TriptyBeta/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TriptyBeta
+//
+//  Created by Miguel Fontamillas on 23/09/2017.
+//  Copyright © 2017 Miguel Fontamillas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
